3aba639f450498364be9abbf0efe10dbcd759b9b
sultanmyrza <sultanmyrza@gmail.com>
2025-06-17 11:37:56 +0800

    Add logging for redirectSystemPath in +native-intent.ts to debug iOS and Android behavior

--- noonexperiments-expo-app/app/+native-intent.ts
+++ noonexperiments-expo-app/app/+native-intent.ts
@@ -1,6 +1,10 @@
 import { getShareExtensionKey } from 'expo-share-intent';
+import { Platform } from 'react-native';
 
 export function redirectSystemPath({ path, initial }: { path: string; initial: string }) {
+  // issue: when sharing from photos app to expo app redirectSystemPath get called on iOS but not on Android
+  console.log(`(${Platform.OS}) redirectSystemPath, path: ${path}, initial: ${initial}`);
+
   try {
     if (path.includes(`dataUrl=${getShareExtensionKey()}`)) {
       // redirect to the ShareIntent Screen to handle data with the hook
