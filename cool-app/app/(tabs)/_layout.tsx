ac7d5d35bb4a2a6f894497547f84c14eaaab8cce
sultanmyrza <sultanmyrza@gmail.com>
2025-09-10 16:59:23 +0800

    Add animation property to tab layout

--- cool-app/app/(tabs)/_layout.tsx
+++ cool-app/app/(tabs)/_layout.tsx
@@ -15,6 +15,7 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
+        animation:'shift'
       }}>
       <Tabs.Screen
         name="index"
